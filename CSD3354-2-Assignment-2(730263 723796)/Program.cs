6c4ea2ab203df5236afcc0a6c0ee9e04eebfc590
visualstudio <visualstudio@122-PC36>
2019-03-06 11:18:44 -0500

    Lab02

--- CSD3354-2-Assignment-2(730263 723796)/Program.cs
+++ CSD3354-2-Assignment-2(730263 723796)/Program.cs
@@ -13,28 +13,33 @@ using System.Threading.Tasks;
 
 namespace CSD3354_2_Assignment_2_730263_723796_
 {
-   
 
-            public class DelegateExercises
+
+
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
         {
-            public delegate void MyDelegate();
-            void Method1()
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+        namespace DelegatesAndEvents
+    {
+        class Program
+        {
+            static void Main(string[] args)
             {
-                Console.WriteLine("Method1");
-                Console.ReadLine();
-            }
-            public void Method2()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
-            class Program
-            {
-                static void Main(string[] args)
-                {
-                    DelegateExercises delegateExercises = new DelegateExercises();
-                    delegateExercises.Method2();
-                }
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method2();
             }
         }
     }
+
+}
